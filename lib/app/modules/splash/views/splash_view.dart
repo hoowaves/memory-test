a289d50cfe9b2565146630b834a698aeb2b3b58e
hoowaves <hoowave@amuz.co.kr>
2024-11-07 12:21:29 +0900

    + logo

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import 'package:get/get.dart';
 
@@ -6,18 +7,21 @@ import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Image.asset('assets/logo/logo.png'),
+            Gap(50),
+            Text("Hoowave Memory Editor", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        )
       ),
     );
   }
